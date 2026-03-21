78fc88f725199d52cb9f13995c22a4b48cfa8e17
pavelgcs <pavel20ganchala@gmail.com>
2026-03-21 12:21:40 -0500

    Clase 8 - Conceptos importantes sobre funciones.

---
+++ ejerciciosClase8/guiaPrograma.cpp
@@ -0,0 +1,31 @@
+/*
+====================== CONCEPTOS IMPORTANTES ===========================
+
+1. Una funcion tiene una tarea especifica
+    No conviene hacer todo en el main.
+    Hay que dividir el programa en partes.
+
+2. Parametros: Son los datos que una funcion recibe.
+    Ejemplo:
+        int calcularSuma(int a, int b, int c, int d)
+
+    Aqui la funcion recibe 4 parametros.
+
+3. Retorno: Valor que la funcion devuelve.
+    Ejemplo:
+        return a + b + c + d;
+
+4. Void: No devuelve un valor, solo realiza una accion.
+
+
+
+
+
+
+
+
+
+
+
+
+*/
