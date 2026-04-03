cb6c10af61e7e16b21f0f93f763b3b4a97fd963e
pavelgcs <pavel20ganchala@gmail.com>
2026-04-03 18:40:39 -0500

    Tarea 13 - Agregar setters y getters con validacion y metodo mostrar

--- java/ejerciciosClase13/ejercicio2Tarea/src/Telefono.java
+++ java/ejerciciosClase13/ejercicio2Tarea/src/Telefono.java
@@ -17,6 +17,41 @@ public class Telefono {
         }
     }
 
-    
+    // GETTERS
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    // SETTERS
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio) {
+        if (precio >= 0) {
+            this.precio = precio;
+        } else {
+            System.out.println("No se puede asignar precio negativo.");
+        }
+    }
+
+    // METODO mostrar
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Precio: $" + precio);
+    }
 
 }
