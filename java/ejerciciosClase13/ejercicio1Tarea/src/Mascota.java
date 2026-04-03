a5beba3bb283a1ed9817070982e05b820dbc7259
pavelgcs <pavel20ganchala@gmail.com>
2026-04-03 10:38:42 -0500

    Tarea 13 - Crear la clase Mascota cons atributos y constructor

---
+++ java/ejerciciosClase13/ejercicio1Tarea/src/Mascota.java
@@ -0,0 +1,22 @@
+public class Mascota {
+
+    // Atributos
+    String nombre;
+    String especie;
+    int edad;
+
+    // Constructor
+    public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }
+
+    // Método para mostrar información
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad);
+    }
+
+}
