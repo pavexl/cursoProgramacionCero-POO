f39aa2656dd6348c35c2a43e0af21001d2afbf6f
pavelgcs <pavel20ganchala@gmail.com>
2026-04-03 10:41:59 -0500

    Tarea 13 - Crear objetos de la clase mascota y mostrar info en app

--- java/ejerciciosClase13/ejercicio1Tarea/src/App.java
+++ java/ejerciciosClase13/ejercicio1Tarea/src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        // Crear objetos
+        Mascota m1 = new Mascota("Firulais", "Perro", 3);
+        Mascota m2 = new Mascota("Michi", "Gato", 2);
+
+        System.out.println("=== Mascota 1 ===");
+        m1.mostrarInfo();
+
+        System.out.println("\n=== Mascota 2 ===");
+        m2.mostrarInfo();
+    }
+        
     }
 }
